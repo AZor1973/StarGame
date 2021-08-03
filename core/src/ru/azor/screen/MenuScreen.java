3716aa6979d7930782516435a6d3f3473668f041
AZor1973 <azor@mail.ru>
2021-08-03 15:30:58 +0300

    lesson2.4

--- core/src/ru/azor/screen/MenuScreen.java
+++ core/src/ru/azor/screen/MenuScreen.java
@@ -1,52 +1,47 @@
 package ru.azor.screen;
 
-
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector2;
 
 import ru.azor.base.BaseScreen;
+import ru.azor.math.Rect;
+import ru.azor.sprite.Background;
 
 public class MenuScreen extends BaseScreen {
 
     private Texture img;
-    private Vector2 pos;
-    private Vector2 v;
-    private Vector2 target;
-    private static final float SPEED = 8f;
+    private Texture bg;
+    private Background background;
+    private static Vector2 v;
+    private static Vector2 target;
 
     @Override
     public void show() {
         super.show();
         img = new Texture("badlogic.jpg");
-        pos = new Vector2();
+        bg = new Texture("textures/bg.png");
+        background = new Background(bg);
         v = new Vector2();
         target = new Vector2();
+    }
+
+    @Override
+    public void resize(Rect worldBounds) {
+        background.resize(worldBounds);
     }
 
     @Override
     public void render(float delta) {
         super.render(delta);
         batch.begin();
-        batch.draw(img, pos.x, pos.y);
+        background.draw(batch);
         batch.end();
-        if (target.dst(pos) > SPEED) {
-            pos.add(v);
-        } else {
-            pos.set(target);
-        }
     }
 
     @Override
     public void dispose() {
         super.dispose();
+        bg.dispose();
         img.dispose();
-    }
-
-    @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        target.set(screenX, Gdx.graphics.getHeight() - screenY);
-        v.set(target.cpy().sub(pos)).setLength(SPEED);
-        return false;
     }
 }
