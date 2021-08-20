c4100d161533421f9b06a496639dfcf917df6557
AZor1973 <azor@mail.ru>
2021-08-20 18:03:14 +0300

    lesson 7.2

--- desktop/src/ru/azor/desktop/DesktopLauncher.java
+++ desktop/src/ru/azor/desktop/DesktopLauncher.java
@@ -2,11 +2,10 @@ package ru.azor.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-
 import ru.azor.StarGame;
 
 public class DesktopLauncher {
-    public static void main(String[] arg) {
+    public static void main (String[] arg) {
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
         config.width = 500;
         config.height = 800;
